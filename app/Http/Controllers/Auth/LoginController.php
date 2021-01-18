e8489db1a283a743bb4daccbbfc7199cc8fa78b7
Patrick <privat@skyraptor.eu>
2021-01-18 21:23:04 +0100

    Update LoginController.php
    Remove unecessary newline

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -54,7 +54,6 @@ class LoginController extends Controller
         $cart = collect($request->session()->get('cart'));
 
         /* Call original logout method */
-        
         $response = $this->originalLogout($request);
 
         /* Repopulate Session with Cart */
